f6502d621ea5f6534c562ac35ea6b1d41c74bf8c
hrajaratnam <hjrajaratnam@icloud.com>
2023-07-17 19:27:04 +0100

    Initial Commit

---
+++ WellnessWizard/WellnessWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessWizardApp.swift
+//  WellnessWizard
+//
+//  Created by Helena Rajaratnam on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WellnessWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
